5c5b9a7a1d620f3e30f8c629406d3716450b0176
Sivanesan <58244919+demoapp527@users.noreply.github.com>
2025-06-08 01:48:51 +0530

    android, IOS support added

--- android/src/main/kotlin/com/webrtc/talklynk_sdk/TalklynkSdkPlugin.kt
+++ android/src/main/kotlin/com/webrtc/talklynk_sdk/TalklynkSdkPlugin.kt
@@ -1,28 +1,35 @@
 package com.webrtc.talklynk_sdk
 
 import androidx.annotation.NonNull
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
 
-class TalklynkSdkPlugin: FlutterPlugin, MethodChannel.MethodCallHandler {
-    private lateinit var channel : MethodChannel
+/** TalklynkSdkPlugin */
+class TalklynkSdkPlugin: FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private lateinit var channel : MethodChannel
 
-    override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-        channel = MethodChannel(binding.binaryMessenger, "talklynk_sdk")
-        channel.setMethodCallHandler(this)
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "talklynk_sdk")
+    channel.setMethodCallHandler(this)
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
     }
+  }
 
-    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: MethodChannel.Result) {
-        if (call.method == "someMethod") {
-            // handle method
-            result.success("response")
-        } else {
-            result.notImplemented()
-        }
-    }
-
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-        channel.setMethodCallHandler(null)
-    }
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
 }
